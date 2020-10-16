8615eef7e914054514d82308d339707541623a77
whitnwalter <Ethan.a.walters1@gmail.com>
2020-10-16 16:01:41 -0600

    Add a class for math functions

---
+++ src/lib/compute.py
@@ -0,0 +1,17 @@
+# Excercise 1: Viewing and Establighing the Status of a File
+
+class Compute:
+    def __init__(self, operator, operands):
+        self.operator = operator
+        self.operands = operands
+
+    def add(self):
+        pass
+
+    def subtract(self):
+        pass
+
+    def multiply(self):
+        pass
+
+    
